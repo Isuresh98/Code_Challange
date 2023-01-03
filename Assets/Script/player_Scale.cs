85047355299af41324bf941f644a54715a401962
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-04 00:09:45 +0530

    Local Scale Change 3

---
+++ Assets/Script/player_Scale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_Scale : MonoBehaviour
+{
+    [SerializeField] private float _scaleAmount;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Scale();
+
+    }//Update
+
+    private void Scale()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            transform.localScale = new Vector3(_scaleAmount, _scaleAmount, _scaleAmount);
+        }
+    }
+}
