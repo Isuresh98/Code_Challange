3c4a39b1705384a24d13424ddebe98ab5a615a03
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-03 23:50:28 +0530

    Local Scale update

--- Assets/Script/Player_Rotetion.cs
+++ Assets/Script/Player_Rotetion.cs
@@ -6,25 +6,33 @@ public class Player_Rotetion : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotation;
     [SerializeField] private float _speed;
-    
+    [SerializeField] private float _scaleAmmount=3f;
+
+
 
     // Update is called once per frame
     void Update()
     {
         Rotetion();
+
     }
 
     private void Rotetion()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.down * _speed * Time.deltaTime);
+            // transform.Rotate(Vector3.down * _speed * Time.deltaTime);
+            transform.localScale = new Vector3(_scaleAmmount, _scaleAmmount, _scaleAmmount);
+
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.up * _speed * Time.deltaTime);
+          transform.Rotate(Vector3.up * _speed * Time.deltaTime);
+           
+
 
         }
+
     }//Rotetion
 }
