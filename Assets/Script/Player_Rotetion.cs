ee1d775ae4d338048145fb852d03ef297aff58f9
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-03 23:35:07 +0530

    update Challange 2

--- Assets/Script/Player_Rotetion.cs
+++ Assets/Script/Player_Rotetion.cs
@@ -6,30 +6,25 @@ public class Player_Rotetion : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotation;
     [SerializeField] private float _speed;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }//Start
+    
 
     // Update is called once per frame
     void Update()
     {
-       
+        Rotetion();
+    }
 
+    private void Rotetion()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            
             transform.Rotate(Vector3.down * _speed * Time.deltaTime);
         }
+
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(Vector3.up * _speed * Time.deltaTime);
 
         }
-
-
-    }//Update
+    }//Rotetion
 }
