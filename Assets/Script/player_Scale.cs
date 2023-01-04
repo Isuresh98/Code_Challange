e45595c6bb2553f2f2e0594bc2d8acc65ba16715
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-04 16:50:23 +0530

    Color Script

--- Assets/Script/player_Scale.cs
+++ Assets/Script/player_Scale.cs
@@ -5,11 +5,43 @@ using UnityEngine;
 public class player_Scale : MonoBehaviour
 {
     [SerializeField] private float _scaleAmount;
-    
+    [SerializeField] private Renderer _renderer;
+  
+
+
+
+    private void Start()
+    {
+           _renderer = GetComponent<Renderer>();
+
+    }
     // Update is called once per frame
     void Update()
     {
         Scale();
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            _renderer.material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+
+            _renderer.material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+
+            _renderer.material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+
+            _renderer.material.color = Color.yellow;
+        }
 
     }//Update
 
