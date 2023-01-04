e6325ca8a607f626c10ced1997840a3dceae7ba8
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-04 17:08:01 +0530

    Color Script Adding

---
+++ Assets/Script/Player_Color_Change.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Color_Change : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer =GetComponent<Renderer>();
+
+
+    }//Start
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorChenger();
+
+    }//Update
+
+    private void ColorChenger()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _renderer.material.color = Color.white;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _renderer.material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _renderer.material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _renderer.material.color = Color.yellow;
+        }
+    }//ColorChenger
+}
