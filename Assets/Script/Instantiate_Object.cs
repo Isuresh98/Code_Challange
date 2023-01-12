a0eb0ffed80ca23117e5352380fae7524861391e
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-12 21:12:41 +0530

    code_Challange-8

---
+++ Assets/Script/Instantiate_Object.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate_Object: MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _horizontal;
+    [SerializeField] private float _vertical;
+
+    private Rigidbody _rbody;
+    private Vector3 _velocityPoint;
+    [SerializeField]
+    private GameObject _prefabs;
+    [SerializeField]
+    private GameObject _spownPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rbody = GetComponent<Rigidbody>();
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        _horizontal = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        _vertical = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+
+        _velocityPoint = _rbody.velocity;
+        _velocityPoint.x = _horizontal;
+        _velocityPoint.z = _vertical;
+        _rbody.velocity = _velocityPoint;
+
+
+    }//Movement
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Point"))
+        {
+
+            Instantiate(_prefabs,_spownPoint.transform.position,_spownPoint.transform.rotation);
+            
+
+        }
+    }//OnCollisionEnter
+
+
+
+}
