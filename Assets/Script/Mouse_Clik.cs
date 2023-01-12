988b465ac353d2f173a487466c55375ec120e7ef
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-12 21:44:34 +0530

    challange 9

---
+++ Assets/Script/Mouse_Clik.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Clik : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitOj;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hitOj))
+            {
+                Destroy(hitOj.collider.gameObject);
+            }
+
+
+        }
+
+
+    }
+}
