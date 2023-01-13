2169756af5fe48b725ee38dc69f5303f09195f53
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-13 12:32:45 +0530

    Challenge 10

---
+++ Assets/Script/Bulat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulat : MonoBehaviour
+{
+    private Rigidbody _rBody;
+    [SerializeField] private float _speed=100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rBody = GetComponent<Rigidbody>();
+     _rBody.AddForce(transform.forward * _speed );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+}
---
+++ Assets/Script/Player_Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _spowPoint;
+    [SerializeField]
+    private GameObject _prfabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_prfabs, _spowPoint.transform.position, _spowPoint.transform.rotation);
+
+        }
+    }
+}
