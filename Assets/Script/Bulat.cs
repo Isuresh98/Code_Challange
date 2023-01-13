e5fc40d3d2d24d4b98be1a4e902e04926159f146
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-13 13:17:49 +0530

    Challenge 11

--- Assets/Script/Bulat.cs
+++ Assets/Script/Bulat.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Bulat : MonoBehaviour
 {
     private Rigidbody _rBody;
+   [SerializeField] private GameObject _spotL;
+    private Light _SpotLComponent;
     [SerializeField] private float _speed=100f;
     // Start is called before the first frame update
     void Start()
     {
         _rBody = GetComponent<Rigidbody>();
      _rBody.AddForce(transform.forward * _speed );
+        _spotL = GameObject.FindWithTag("Spot1");
+        _SpotLComponent = _spotL.GetComponent<Light>();
+        _SpotLComponent.enabled = false;
+        
 
     }
 
@@ -19,6 +25,20 @@ public class Bulat : MonoBehaviour
     {
        
 
+
+    }
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Point"))
+        {
+            _SpotLComponent.enabled = true;
+            print("hit");
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+            _SpotLComponent.enabled = false;
 
     }
 }
