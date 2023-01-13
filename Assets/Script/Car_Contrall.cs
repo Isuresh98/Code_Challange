dfcdbb92f34584c149d927723f75b4174833ccc4
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-13 14:00:50 +0530

    Challange 12

---
+++ Assets/Script/Car_Contrall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Contrall : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotetSpeed;
+
+    [SerializeField] private float _horizontal;
+    [SerializeField] private float _vertical;
+
+    private float drix;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+    }
+
+    private void Movement()
+    {
+        _horizontal = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        _vertical = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        transform.Rotate(Vector3.up * _horizontal * _rotetSpeed * Time.deltaTime);
+        transform.Translate(0f, 0f, _vertical * _speed * Time.deltaTime);
+    }
+}
