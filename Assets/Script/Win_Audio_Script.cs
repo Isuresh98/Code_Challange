ac6307cb866635b4dc7c4dc45c6f4d94bc4d60c5
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-16 22:33:26 +0530

    15

---
+++ Assets/Script/Win_Audio_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_Audio_Script : MonoBehaviour
+{
+    private AudioSource _winSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _winSound = GetComponent<AudioSource>();
+        _winSound.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            _winSound.Play();
+        }
+    }
+}
